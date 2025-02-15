c054ada0155ae753e76c23e732198494f64c8b06
Edward Tao <jiayou_tao@apple.com>
2025-02-15 03:05:30 -0800

    Create the Spring boot project framework with Maven Build | Create gitignore

---
+++ src/test/java/com/tjm/content_management_app/ContentManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjm.content_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
