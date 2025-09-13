fdf47cbb54c43824edf728660aa4a6052748f53c
Edward Tao <jiayou_tao@apple.com>
2025-09-12 22:20:31 -0700

    fix CORS

--- tjm-web-app/src/main/java/com/tjm/tjmwebapp/controllers/TJMNewsController.java
+++ tjm-web-app/src/main/java/com/tjm/tjmwebapp/controllers/TJMNewsController.java
@@ -6,10 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -17,6 +14,12 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/tjm/api/v1")
+@CrossOrigin(
+        origins = {
+                "http://localhost:3000",
+                "https://tjm-usa-client-ui.netlify.app"  // prod UI
+        }
+)
 public class TJMNewsController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(TJMNewsController.class);
@@ -38,6 +41,4 @@ public class TJMNewsController {
         LOGGER.info("GET /tjm/api/v1/news/articles/{}", slug);
         return tjmNewsService.getArticleBySlug(slug);
     }
-
-
 }
